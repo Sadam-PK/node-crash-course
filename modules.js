79d55c46a2deacffe7e803e724dfe4b59a5cbb3c
sadam <sadamccr@gmail.com>
2023-01-07 01:35:56 +0500

    importing done

---
+++ modules.js
@@ -0,0 +1,10 @@
+const xyz = require('./people');
+
+// //through destructuring..
+
+const {people} = require('./people')
+
+// console.log(xyz);
+
+// console.log(xyz.people, xyz.ages);
+console.log(people);
---
+++ people.js
@@ -0,0 +1,10 @@
+const people = ['sadam', 'hamza', 'asad']
+const ages = [26, 16, 14]
+
+// console.log(people);
+
+// module.exports = people
+
+module.exports = {
+    people, ages
+}
