90b946ec97a60d94d3aa26e352507fee0ff8a7ee
sadam <sadamccr@gmail.com>
2023-01-08 01:25:47 +0500

    server js file added

--- streams.js
+++ streams.js
@@ -1,13 +1,13 @@
-const fs = require('fs')
-const readStream = fs.createReadStream('./docs/blog2.txt')
-const writeStream = fs.createWriteStream('./docs/blog3.txt');
+// const fs = require('fs')
+// const readStream = fs.createReadStream('./docs/blog2.txt')
+// const writeStream = fs.createWriteStream('./docs/blog3.txt');
 
-readStream.on('data', (chunk) => {
-    console.log('---new chunk----');
-    console.log(chunk.toString());
+// readStream.on('data', (chunk) => {
+//     console.log('---new chunk----');
+//     console.log(chunk.toString());
 
-    writeStream.write('\nNew Chunk\n')
-    writeStream.write(chunk)
-});
+//     writeStream.write('\nNew Chunk\n')
+//     writeStream.write(chunk)
+// });
 
 
