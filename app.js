87fb84fee02d0a644d56c4339a60916b8af15b95
sadam <sadamccr@gmail.com>
2023-01-15 00:34:59 +0500

    connected to mongodb

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 const app = express();
 
 //connect to mongodb
-const dbURI = 'mongodb+srv://sadam:<password>@cluster0.gaothmf.mongodb.net/?retryWrites=true&w=majority'
+const dbURI = 'mongodb+srv://sadam:test1234@cluster0.gaothmf.mongodb.net/nodejs?retryWrites=true&w=majority'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err))
 
 app.set('view engine', 'ejs')
 
-app.listen(3000);
 app.use(morgan('dev'));
 app.use(express.static('public'))
 app.get('/', (req, res) => {
